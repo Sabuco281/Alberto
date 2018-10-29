c7f8eecae4e9e39c04f5055bb3a7f42eab036839
Blanco <Blanco@Blanco-PC>
2018-10-29 18:30:12 +0100

    numericos

---
+++ src/alberto/Alberto.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alberto;
+
+/**
+ *
+ * @author Blanco
+ */
+public class Alberto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] numericos) {
+         
+
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+    }
+    
+}
